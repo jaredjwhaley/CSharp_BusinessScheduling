b98d742df9239a0f6651a05410589305238319cc
Jared Whaley <jaredjwhaley@gmail.com>
2026-02-27 22:41:02 -0600

    - Replaced Entities.Person's composition PhotoId Property with a DDD compliant PhotoIdGuid Property and adjusted all affected functionality - Added XML documentation to Entities.Person

--- BusinessScheduling.Domain/Entities/Person.cs
+++ BusinessScheduling.Domain/Entities/Person.cs
@@ -20,7 +20,13 @@ namespace BusinessScheduling.Domain.Entities
         public string PhoneNumber { get; set; }
 
         // Value objects
-        public PhotoId PhotoId { get; set; }
+        /// <summary>
+        /// The unique identifier for the <see cref="PhotoId"> on file for this client.
+        /// </summary>
+        public Guid PhotoIdGuid { get; set; }
+        /// <summary>
+        /// Gets or sets the street address information associated with the entity.
+        /// </summary>
         public Address Address { get; set; }
 
         // Calculated properties
@@ -36,34 +42,35 @@ namespace BusinessScheduling.Domain.Entities
         public string FullName { get => $"{FirstName} {LastName}".Trim(); }
 
         /// <summary>
-        /// Initializes a new instance of the Person class with the specified personal and contact details.
+        /// Initializes a new instance of the Person class with the specified personal details, contact information, and
+        /// optional identifiers.
         /// </summary>
-        /// <param name="photoId">The unique identifier for the person's photo. Cannot be null.</param>
         /// <param name="firstName">The first name of the person. Cannot be null.</param>
         /// <param name="lastName">The last name of the person. Cannot be null.</param>
         /// <param name="dateOfBirth">The date of birth of the person. Cannot be null.</param>
-        /// <param name="address">The address information for the person. Cannot be null.</param>
-        /// <param name="email">The email address of the person. Defaults to an empty string if not provided.</param>
-        /// <param name="phoneNumber">The phone number of the person. Defaults to an empty string if not provided.</param>
-        /// <param name="id">An optional unique identifier for the person. Included for possible rehydration from a database. If not provided, a new GUID is generated.</param>
-        /// <exception cref="ArgumentNullException">Thrown if photoId, firstName, lastName, dateOfBirth, or address is null.</exception>
+        /// <param name="address">The address associated with the person. Cannot be null.</param>
+        /// <param name="email">The email address of the person. If not specified, defaults to an empty string.</param>
+        /// <param name="phoneNumber">The phone number of the person. If not specified, defaults to an empty string.</param>
+        /// <param name="photoIdGuid">An optional GUID representing the person's photo ID. If not specified, defaults to an empty GUID. This is required for appointments.</param>
+        /// <param name="id">An optional unique identifier for the person. If not specified, a new GUID is generated.</param>
+        /// <exception cref="ArgumentNullException">Thrown if firstName, lastName, dateOfBirth, or address is null.</exception>
         public Person(
-            PhotoId photoId,
             string firstName,
             string lastName,
             DateOnly ?dateOfBirth,
             Address address,
             string email = "",
             string phoneNumber = "",
-            Guid ?id = null)
+            Guid? photoIdGuid = null,
+            Guid? id = null)
         {
-            PhotoId = photoId ?? throw new ArgumentNullException(nameof(photoId));
             FirstName = firstName ?? throw new ArgumentNullException(nameof(firstName));
             LastName = lastName ?? throw new ArgumentNullException(nameof(lastName));
             DateOfBirth = dateOfBirth ?? throw new ArgumentNullException(nameof(dateOfBirth));
             Address = address ?? throw new ArgumentNullException(nameof(address));
             Email = email;
             PhoneNumber = phoneNumber;
+            PhotoIdGuid = photoIdGuid ?? Guid.Empty;
             Id = id ?? Guid.NewGuid();
         }
     }
