dad2d3bdf5a80a97dc2f436bf7beb5326f28f158
Jared Whaley <jaredjwhaley@gmail.com>
2026-03-01 21:23:51 -0600

    - Fulfilment of PersonTest tipe coverage for currently implemented functionality

--- BusinessScheduling.Tests/Domain/Entities/PersonTests.cs
+++ BusinessScheduling.Tests/Domain/Entities/PersonTests.cs
@@ -1,102 +1,106 @@
-﻿using BusinessScheduling.Domain.ValueObjects;
-using BusinessScheduling.Domain.Entities;
+﻿using BusinessScheduling.Domain.Entities;
+using BusinessScheduling.Domain.ValueObjects;
 using Xunit;
 
 namespace BusinessScheduling.Tests.Domain.Entities
 {
-    public class PersonTests : IDisposable
+    public class PersonTests
     {
-        #region Setup and Teardown
+        private readonly string _firstName = "John";
+        private readonly string _lastName = "Doe";
+        private readonly DateOnly _dob = DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-30));
+        private readonly string _email = "john@doe.com";
+        private readonly string _phone = "555-1234";
+        private readonly Address _address =
+            new Address("123 Main St", "Anytown", UsState.AZ, "12345");
 
-        // Identifier
-        private Guid _id;
-
-        // Core personal info
-        private DateOnly _dob;
-        private string _firstName;
-        private string _lastName;
-        private string _email;
-        private string _phoneNumber;
-
-        // Value objects
-        private PhotoId? _photoId;
-        private Address _address;
-
-        public PersonTests() {
-            _id = Guid.NewGuid();
-            _firstName = "John";
-            _lastName = "Doe";
-            _dob = DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-30)); // 30 years old
-            _email = "johndoe@somesite.org";
-            _phoneNumber = "555-123-4567";
-            _photoId = new PhotoId("validFilename.jpg", DateOnly.FromDateTime(DateTime.UtcNow.AddYears(2)));
-            _address = new Address("123 Main St", "Anytown", UsState.AZ, "12345");
-        }
-        public void Dispose()
-        {
-            // TODO: ChatGPT, explain this to me please. I get that it's for
-            // teardown, but I'm not sure what, if anything, needs to be
-            // explicitly removed as part of the dispose method.
-        }
-        #endregion
+        private readonly PhotoId _photoId =
+            new PhotoId("id.jpg", DateOnly.FromDateTime(DateTime.UtcNow.AddYears(2)));
 
         #region Constructor Tests
+
         [Fact]
-        public void Constructor_WhenGivenValidValues_CreatesInstance()
+        public void Constructor_WhenValid_CreatesPerson()
         {
-            // Act
             var person = new Person(
-                firstName: _firstName,
-                lastName: _lastName,
-                dateOfBirth: _dob,
-                address: _address,
-                email: _email,
-                phoneNumber: _phoneNumber,
-                photoId: _photoId,
-                id: _id
+                _firstName,
+                _lastName,
+                _dob,
+                _address,
+                _email,
+                _phone,
+                _photoId
             );
-            // Assert
-            Assert.Equal(_id, person.Id);
+
             Assert.Equal(_firstName, person.FirstName);
             Assert.Equal(_lastName, person.LastName);
             Assert.Equal(_dob, person.DateOfBirth);
-            Assert.Equal(_email, person.Email);
-            Assert.Equal(_phoneNumber, person.PhoneNumber);
-            Assert.Equal(_photoId, person.PhotoId);
             Assert.Equal(_address, person.Address);
-        }
-
-        // Enumerable data for invalid constructor tests.
-        // Required to test multiple null parameters utilizing MemberData
-        // without needing to write multiple [Fact] tests
-        public static IEnumerable<object[]> InvalidConstructorData()
-        {
-            var validDob = DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-30));
-            var validAddress = new Address("123 Main St", "Anytown", UsState.AZ, "12345");
-            var validPhotoId = new PhotoId("valid.jpg", DateOnly.FromDateTime(DateTime.UtcNow.AddYears(1)));
-            var validId = Guid.NewGuid();
-
-            yield return new object[] { null, "Doe", validDob, validAddress, "a@b.com", "555", validPhotoId, validId };
-            yield return new object[] { "John", null, validDob, validAddress, "a@b.com", "555", validPhotoId, validId };
-            yield return new object[] { "John", "Doe", null, validAddress, "a@b.com", "555", validPhotoId, validId };
-            yield return new object[] { "John", "Doe", validDob, null, "a@b.com", "555", validPhotoId, validId };
-        }
-
-        [Theory]
-        [MemberData(nameof(InvalidConstructorData))]
-        public void Constructor_WhenRequiredArgumentsAreNull_ThrowsArgumentNullException(
-            string firstName, string lastName, DateOnly? dateOfBirth, Address address,
-            string email, string phoneNumber, PhotoId? photoId, Guid id)
-        {
-            Assert.Throws<ArgumentNullException>(() =>
-                new Person( firstName, lastName, dateOfBirth, address,
-                    email, phoneNumber, photoId, id
-                ));
+            Assert.Equal(_email, person.Email);
+            Assert.Equal(_phone, person.PhoneNumber);
+            Assert.Equal(_photoId, person.PhotoId);
+            Assert.NotEqual(Guid.Empty, person.Id);
         }
 
         #endregion
 
-        #region ???
+        #region Derived Property Tests
+
+        [Fact]
+        public void Age_WhenBirthdayHasPassed_IsCorrect()
+        {
+            var dob = DateOnly.FromDateTime(DateTime.UtcNow.AddYears(-30).AddDays(-1));
+            var person = new Person(_firstName, _lastName, dob, _address);
+
+            Assert.Equal(30, person.Age);
+        }
+
+        [Fact]
+        public void FullName_ReturnsFirstAndLast()
+        {
+            var person = new Person(_firstName, _lastName, _dob, _address);
+
+            Assert.Equal("John Doe", person.FullName);
+        }
+
+        [Fact]
+        public void FullName_WhenLastNameMissing_StillFormats()
+        {
+            var person = new Person("John", "", _dob, _address);
+
+            Assert.Equal("John", person.FullName);
+        }
+
+        #endregion
+
+        #region Id Tests
+
+        [Fact]
+        public void Constructor_WhenIdProvided_UsesIt()
+        {
+            var id = Guid.NewGuid();
+
+            var person = new Person(
+                _firstName,
+                _lastName,
+                _dob,
+                _address,
+                id: id
+            );
+
+            Assert.Equal(id, person.Id);
+        }
+
+        [Fact]
+        public void Constructor_WhenIdNotProvided_GeneratesNewId()
+        {
+            var person1 = new Person(_firstName, _lastName, _dob, _address);
+            var person2 = new Person(_firstName, _lastName, _dob, _address);
+            Assert.NotEqual(Guid.Empty, person1.Id);
+            Assert.NotEqual(Guid.Empty, person2.Id);
+            Assert.NotEqual(person1.Id, person2.Id);
+        }
+
         #endregion
     }
 }
